423d340affb9f3f0d846e56ec3e1bcff9f04417c
Caleb Li <1caleblili@gmail.com>
2022-06-22 20:49:53 -0700

    react!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState, useEffect } from 'react'
+/*
+const Person = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastname}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+*/
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert('Youve changed the counter to ' + counter)
+  }, [counter]);
+  
+  return (
+    <div className='App'>
+      <button onClick = {() => setCounter((prevCounter) => prevCounter - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick = {() => setCounter((nextCounter) => nextCounter + 1)}>+</button>
     </div>
   );
 }
